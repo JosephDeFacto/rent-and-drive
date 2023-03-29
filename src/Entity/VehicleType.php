a45a8f45f9ab75ab863d11f20ca0ac5c9d54bafc
JosephDeFacto <josip.mareljic@gmail.com>
2023-03-29 21:21:00 +0200

    Add new entity VehicleType for browsing cars by vehicle type

---
+++ src/Entity/VehicleType.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+use App\Repository\VehicleTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VehicleTypeRepository::class)
+ */
+class VehicleType
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private ?string $nameType;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Car::class, mappedBy="vehicleType")
+     */
+    private $cars;
+
+    public function __construct()
+    {
+        $this->cars = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameType(): ?string
+    {
+        return $this->nameType;
+    }
+
+    public function setNameType(?string $nameType): self
+    {
+        $this->nameType = $nameType;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Car>
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): self
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars[] = $car;
+            $car->setVehicleType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): self
+    {
+        if ($this->cars->removeElement($car)) {
+            // set the owning side to null (unless already changed)
+            if ($car->getVehicleType() === $this) {
+                $car->setVehicleType(null);
+            }
+        }
+
+        return $this;
+    }
+}
