fe4c6486a3f920cb0032899714569d9c7c7facd3
JosephDeFacto <josip.mareljic@gmail.com>
2023-04-09 18:30:18 +0200

    Remove json endpoint

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,97 +2,53 @@
 
 namespace App\Controller;
 
-use App\Entity\Brand;
-use App\Entity\Car;
-use App\Entity\VehicleType;
+use App\Repository\BrandRepository;
 use App\Repository\CarRepository;
-use App\Validator\Validator;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\VehicleTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class CarController extends AbstractController
 {
+
     private CarRepository $carRepository;
-    private Validator $validator;
+    private VehicleTypeRepository $vehicleTypeRepository;
+    private BrandRepository $brandRepository;
 
 
-    public function __construct(CarRepository $carRepository, Validator $validator)
+
+    public function __construct(CarRepository $carRepository, BrandRepository $brandRepository, VehicleTypeRepository $vehicleTypeRepository)
     {
         $this->carRepository = $carRepository;
-        $this->validator = $validator;
+        $this->brandRepository = $brandRepository;
+        $this->vehicleTypeRepository = $vehicleTypeRepository;
     }
 
     /**
      * @Route("/cars", name="app_cars")
      */
-    public function index(Request $request, ManagerRegistry $registry, ValidatorInterface $validator): ?Response
+    public function index(): Response
     {
-        $vehicleType = $request->get('vehicleType');
+        $cars = $this->carRepository->findAll();
 
-        $brand = $request->get('brand');
+        $vehicleTypes = $this->vehicleTypeRepository->findAll();
 
-        $criteria = [];
+        $brands = $this->brandRepository->findAll();
 
-        if ($vehicleType) {
-            $criteria['vehicleType'] = $vehicleType;
-        } else if ($brand) {
-            $criteria['brand'] = $brand;
-        }
-
-        $vehicleTypes = $registry->getRepository(VehicleType::class)->findAll();
-        $brands = $registry->getRepository(Brand::class)->findAll();
-
-        if ($request->isXmlHttpRequest()) {
-
-            if ($vehicleType || $brand) {
-                $cars = $this->carRepository->findBy($criteria);
-            } else {
-                $cars = $this->carRepository->findAll();
-            }
-
-            $responseData = [];
-            if (isset($cars)) {
-                foreach ($cars as $car) {
-                    $responseData[] = [
-                        'id' => $car->getId(),
-                        'name' => $car->getName(),
-                        'model' => $car->getModel(),
-                        'imagePath' => $car->getImagePath(),
-                    ];
-                }
-            }
-
-            return new JsonResponse($responseData);
-        } else {
-
-            $allCars = $this->carRepository->findAll();
-
-            if ($vehicleType || $brand) {
-                $cars = $this->carRepository->findBy($criteria);
-            } else {
-                $cars = $allCars;
-            }
-
-            return $this->render('car/index.html.twig', [
-                'cars' => $cars,
-                'types' => $vehicleTypes,
-                'brands' => $brands,
-            ]);
-        }
+        return $this->render('car/index.html.twig', [
+            'cars' => $cars,
+            'types' => $vehicleTypes,
+            'brands' => $brands,
+        ]);
     }
 
     /**
      * @Route("/show/{id}", name="car_show", methods={"GET"})
      */
-    public function show(Car $car): Response
+    public function show($id): Response
     {
-        $car = $this->carRepository->find($car);
+        $car = $this->carRepository->find($id);
 
         return $this->render('car/show.html.twig', ['carInfo' => $car]);
     }
