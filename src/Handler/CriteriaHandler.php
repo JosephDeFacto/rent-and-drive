0435c83a7ce5b99da7fe009d888ebdedb30edef7
JosephDeFacto <josip.mareljic@gmail.com>
2023-05-23 21:20:42 +0200

    Handle filter criteria

---
+++ src/Handler/CriteriaHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Handler;
+
+use App\Requests\RequestDataExtractor;
+
+class CriteriaHandler
+{
+    private RequestDataExtractor $extractor;
+
+    public function __construct(RequestDataExtractor $extractor)
+    {
+        $this->extractor = $extractor;
+    }
+
+    public function paramsCriteria(): array
+    {
+        return [
+            'vehicleType' => $this->extractor->getRequestValue('vehicleType'),
+            'brand' => $this->extractor->getRequestValue('brand'),
+        ];
+    }
+    public function handleCriteria(array $params): array
+    {
+        $criteria = [];
+
+        foreach ($params as $key => $value) {
+            if ($value) {
+                $criteria[$key] = $value;
+            }
+        }
+
+        return $criteria;
+    }
+}
